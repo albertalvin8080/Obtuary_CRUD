fdc652e646ab6061ff6cfb877854cd3ea4b8e429
AlbertAlvi <albertalvin99@gmail.com>
2024-03-16 01:04:14 -0300

    english_translation

--- obtuary.c
+++ obtuary.c
@@ -2,310 +2,359 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-#define MAX_MORTOS 1000
+#define MAX_DEAD 1000
 
-// Indice Global - quantidade de mortos
-size_t gMortos = 0;
+// Global Index - dead count
+size_t gDeadCount = 0;
 
-typedef struct{
-    int dia,mes,ano;
-}Data;
+typedef struct
+{
+	int day, month, year;
+} Date;
 
-typedef struct{
-    Data dataMorte;
-	Data dataNascimento;
-    char nomeMorto[64];
-	char causaMorte[128];
-	char descricaoMorto[128];
-}Obituario;
+typedef struct
+{
+	Date deathDate;
+	Date birthDate;
+	char deadName[64];
+	char deathCause[128];
+	char description[128];
+} Obtuary;
 
-// *inversao dos valores das primeiras duas telas
-typedef enum{
-    TELA_PRINCIPAL = -1,
-    TELA_SAIR,        //0
-    TELA_CADASTRAR,   //1
-    TELA_DESCADASTRAR,//2
-    TELA_LISTAR_TODOS //3
-}Telas;
+typedef enum
+{
+	MAIN_SCREEN = -1,
+	OUT_SCREEN,		   // 0
+	REGISTER_SCREEN,   // 1
+	UNREGISTER_SCREEN, // 2
+	LIST_ALL_SCREEN	   // 3
+} Screen;
 
-void limparBuffer(void);
-void listarTodos(Obituario *obit);
-void salvarEmDisco(Obituario *obit);
-void cadastrarRegistro(Obituario *obit);
-int compararFalecimento(Obituario *obit);
-void descadastrarRegistro(Obituario *obit);
-int checarDatas(int dia, int mes, int ano);
-void carregarRegistrosParaMemoria(Obituario *obit);
+void cleanBuffer(void);
+void listAll(Obtuary *obit);
+void saveToDisk(Obtuary *obit);
+void registerDead(Obtuary *obit);
+int compareDeath(Obtuary *obit);
+void unregisterDead(Obtuary *obit);
+int checkDates(int day, int month, int year);
+void loadRecordsToProgramMemory(Obtuary *obit);
 
 int main()
 {
-	// permite o printf() de caracteres acentuados
-	setlocale(LC_ALL,"Portuguese_Brazil");
-	
-    Telas menu;
-	// usar calloc() no lugar de memset()
-    Obituario *obit = (Obituario*) calloc(MAX_MORTOS,sizeof(Obituario));
-    
-    // coloca os registros na memoria do programa
-    carregarRegistrosParaMemoria(obit);
-    
-    printf("===========================================\n");
-    printf("\t\tOBITUÁRIO\n");
-    printf("===========================================\n\n");
-    
-    do{
-    	printf("-------------------------------------------\n");
-    	printf("\t      TELA PRINCIPAL\n");
-    	printf("-------------------------------------------\n");
-    	printf("-Menu de Escolhas-\n\t");
-    	printf("1 -> Cadastrar registro;\n\t");
-    	printf("2 -> Descadastrar registro;\n\t");
-    	printf("3 -> Listar todos os registros;\n\t");
-    	printf("0 -> Encerra programa.\n\t?");
-        	scanf("%d",&menu);
-        	limparBuffer();
-    	
-    	switch(menu){
-    		case TELA_CADASTRAR:
-    			{
-    				cadastrarRegistro(obit);	
-    				menu = TELA_PRINCIPAL;
-				}
-    			break;
-    		
-    		case TELA_DESCADASTRAR:
-    			{
-    				descadastrarRegistro(obit);
-    				menu = TELA_PRINCIPAL;
-				}
-    			break;
-    		
-    		case TELA_LISTAR_TODOS:
-    			{
-    				listarTodos(obit);
-    				menu = TELA_PRINCIPAL;
-				}
-    			break;
-    		
-    		case TELA_SAIR:
-    			{
-    				salvarEmDisco(obit);
-    				printf("===========================================\n");
-    				printf("\t\tFINALIZADO\n");
-    				printf("===========================================");
-				}
-    			break;
-    		
-    		default:
-    			{
-    				printf("*Opção inválida.\n");
-    				menu = TELA_PRINCIPAL;
-				}
-    			break;
+	// setlocale(LC_ALL,"Portuguese_Brazil");
+
+	Screen menu;
+	Obtuary *obit = (Obtuary *)calloc(MAX_DEAD, sizeof(Obtuary));
+	// Obtuary *obit = (Obtuary *)malloc(MAX_DEAD * sizeof(Obtuary));
+	// memset(obit, 0, MAX_DEAD * sizeof(Obtuary));
+
+	loadRecordsToProgramMemory(obit);
+
+	printf("===========================================\n");
+	printf("\t\tOBTUARY\n");
+	printf("===========================================\n\n");
+
+	do
+	{
+		printf("-------------------------------------------\n");
+		printf("\t      MAIN SCREEN\n");
+		printf("-------------------------------------------\n");
+		printf("\t-MENU-\n\t");
+		printf("1 -> Register;\n\t");
+		printf("2 -> Unregister;\n\t");
+		printf("3 -> List all;\n\t");
+		printf("0 -> End session.\n\t?");
+		scanf("%d", &menu);
+		cleanBuffer();
+
+#ifdef _WIN32
+		system("cls");
+#else
+		system("clear");
+#endif
+
+		switch (menu)
+		{
+		case REGISTER_SCREEN:
+		{
+			registerDead(obit);
+			menu = MAIN_SCREEN;
+		}
+		break;
+
+		case UNREGISTER_SCREEN:
+		{
+			unregisterDead(obit);
+			menu = MAIN_SCREEN;
+		}
+		break;
+
+		case LIST_ALL_SCREEN:
+		{
+			listAll(obit);
+			menu = MAIN_SCREEN;
+		}
+		break;
+
+		case OUT_SCREEN:
+		{
+			saveToDisk(obit);
+			printf("===========================================\n");
+			printf("\t\t  END\n");
+			printf("===========================================");
+		}
+		break;
+
+		default:
+		{
+			printf("*Invalid Option\n");
+			menu = MAIN_SCREEN;
+		}
+		break;
 		}
 		printf("\n");
-    	
-	} while(menu == TELA_PRINCIPAL);
-    
-    // liberar alocacao antes de terminar
+
+	} while (menu == MAIN_SCREEN);
+
 	free(obit);
-	
-    return(EXIT_SUCCESS);
+
+	return (EXIT_SUCCESS);
 }
 
-void salvarEmDisco(Obituario *obit){
+void saveToDisk(Obtuary *obit)
+{
 	FILE *f;
-		f = fopen("obituario.bin","wb");
-		
-		size_t salvos = fwrite(obit,sizeof(Obituario),gMortos,f);
-		if(salvos != gMortos){
-			printf("\n*ERRO: falha ao salvar em disco.");
-			exit(EXIT_FAILURE);
-		}
-		
-		fclose(f);
+	f = fopen("obtuary.bin", "wb");
+
+	size_t salvos = fwrite(obit, sizeof(Obtuary), gDeadCount, f);
+	if (salvos != gDeadCount)
+	{
+		printf("\n*ERROR: failed to persist data.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(f);
 }
 
-void descadastrarRegistro(Obituario *obit){
-	int indice;
-	if(gMortos == 0){
-		printf("\nSEM REGISTROS PARA DESCADASTRAR.\n");
-	}else{
+void unregisterDead(Obtuary *obit)
+{
+	int index;
+	if (gDeadCount == 0)
+	{
+		printf("\nNOTHING TO UNREGISTER.\n");
+	}
+	else
+	{
 		printf("-------------------------------------------\n");
-    	printf("\t      TELA DE EXCLUSÃO\n");
-    	printf("-------------------------------------------");
-    	
-    	while(1){
-			printf("\nInsira um índice (entre 0 e %zd) para remover: ",gMortos-1);
-				scanf("%d",&indice);
-				limparBuffer();
-			
-			if(indice < 0 || indice > gMortos-1){
-				printf("\n*Índice inválido. Tente novamente.");
-			}else{
+		printf("\t      EXCLUSION SCREEN\n");
+		printf("-------------------------------------------");
+
+		while (1)
+		{
+			printf("\nChoose and index between 0 and %zd to remove: ", gDeadCount - 1);
+			scanf("%d", &index);
+			cleanBuffer();
+
+			if (index < 0 || index > gDeadCount - 1)
+			{
+				printf("\n*Invalid Index. Try again.");
+			}
+			else
+			{
 				break;
 			}
 		}
-		
-		for(int i = indice; i < gMortos - 1; i++){
-			obit[i] = obit[i+1];
+
+		for (int i = index; i < gDeadCount - 1; i++)
+		{
+			obit[i] = obit[i + 1];
 		}
-		gMortos--;
-		
-		printf("REGISTRO EXCLUIDO COM SUCESSO.\n");
+		gDeadCount--;
+
+		printf("\nUNREGISTERED SUCCESSFULLY.\n");
 	}
 }
 
-void cadastrarRegistro(Obituario *obit){
-    if(gMortos>=MAX_MORTOS){
-		printf("\nOBITUÁRIO CHEIO. IMPOSSÍVEL ADICIONAR.\n");
-	}else{
+void registerDead(Obtuary *obit)
+{
+	if (gDeadCount >= MAX_DEAD)
+	{
+		printf("\n*WARNING: obtuary is full.\n");
+	}
+	else
+	{
 		printf("\n-------------------------------------------\n");
-	    printf("\t     TELA DE CADASTRO\n");
-	    printf("-------------------------------------------");
-	    
-		char confirma;
-		do{
-			printf("\nInsira o nome: ");
-				scanf("%64[^\n]",obit[gMortos].nomeMorto);
-				limparBuffer();
-			printf("Insira a causa da morte: ");
-				scanf("%128[^\n]",obit[gMortos].causaMorte);
-				limparBuffer();
-			printf("Agora uma breve descrição: ");
-				scanf("%128[^\n]",obit[gMortos].descricaoMorto);
-				limparBuffer();
-			
-			while(1){
-				printf("Insira a data de Nascimento ->\nDia: ");
-					scanf("%d",&obit[gMortos].dataNascimento.dia);
-					limparBuffer();
-				printf("Mes: ");
-					scanf("%d",&obit[gMortos].dataNascimento.mes);
-					limparBuffer();
-				printf("Ano: ");
-					scanf("%d",&obit[gMortos].dataNascimento.ano);
-					limparBuffer();
-				
-				if( checarDatas(
-				obit[gMortos].dataNascimento.dia,
-				obit[gMortos].dataNascimento.mes,
-				obit[gMortos].dataNascimento.ano) == 1 ){
-					printf("\n*Data inválida inserida. Tente novamente.\n");
-				}else{
+		printf("\t     REGISTER SCREEN\n");
+		printf("-------------------------------------------");
+
+		char confirm;
+		do
+		{
+			printf("\nInsert the name: ");
+			scanf("%64[^\n]", obit[gDeadCount].deadName);
+			cleanBuffer();
+			printf("Insert death cause: ");
+			scanf("%128[^\n]", obit[gDeadCount].deathCause);
+			cleanBuffer();
+			printf("Insert description to the deceased: ");
+			scanf("%128[^\n]", obit[gDeadCount].description);
+			cleanBuffer();
+
+			while (1)
+			{
+				printf("Insert birth date ->\nDay: ");
+				scanf("%d", &obit[gDeadCount].birthDate.day);
+				cleanBuffer();
+				printf("Month: ");
+				scanf("%d", &obit[gDeadCount].birthDate.month);
+				cleanBuffer();
+				printf("Year: ");
+				scanf("%d", &obit[gDeadCount].birthDate.year);
+				cleanBuffer();
+
+				if (checkDates(
+						obit[gDeadCount].birthDate.day,
+						obit[gDeadCount].birthDate.month,
+						obit[gDeadCount].birthDate.year) == 1)
+				{
+					printf("\n*Invalid date. Try again\n");
+				}
+				else
+				{
 					break;
 				}
 			}
-			
-			while(1){
-				printf("Insira a data do Falecimento ->\nDia: ");
-					scanf("%d",&obit[gMortos].dataMorte.dia);
-					limparBuffer();
-				printf("Mes: ");
-					scanf("%d",&obit[gMortos].dataMorte.mes);
-					limparBuffer();
-				printf("Ano: ");
-					scanf("%d",&obit[gMortos].dataMorte.ano);
-					limparBuffer();	
-					
-				if( checarDatas(
-				obit[gMortos].dataMorte.dia,
-				obit[gMortos].dataMorte.mes,
-				obit[gMortos].dataMorte.ano) == 1 ){
-					printf("\n*Data inválida inserida. Tente novamente.\n");
-				}else if( compararFalecimento(obit) == 1 ){
-					printf("\n*O falecido não pode nascer antes de sua morte.\n");
-				}else{
+
+			while (1)
+			{
+				printf("Insert date of death ->\nDay: ");
+				scanf("%d", &obit[gDeadCount].deathDate.day);
+				cleanBuffer();
+				printf("Month: ");
+				scanf("%d", &obit[gDeadCount].deathDate.month);
+				cleanBuffer();
+				printf("Year: ");
+				scanf("%d", &obit[gDeadCount].deathDate.year);
+				cleanBuffer();
+
+				if (checkDates(
+						obit[gDeadCount].deathDate.day,
+						obit[gDeadCount].deathDate.month,
+						obit[gDeadCount].deathDate.year) == 1)
+				{
+					printf("\n*Invalid date. Try again\n");
+				}
+				else if (compareDeath(obit) == 1)
+				{
+					printf("\n*The deceased cannot have died before it's birth.\n");
+				}
+				else
+				{
 					break;
 				}
 			}
-			
-			printf("\nDeseja mesmo armazenar novo registro?\n(s/n): ");
-				scanf("%c",&confirma);
-		} while(confirma == 'n' || confirma == 'N');
-		
-		printf("REGISTRO ADICIONADO COM SUCESSO.\n");
-		
-		gMortos++;
+
+			printf("\nAre you sure?\n(y/n): ");
+			scanf("%c", &confirm);
+		} while (confirm == 'n' || confirm == 'N');
+
+		printf("\nREGISTERED SUCCESSFULLY.\n");
+
+		gDeadCount++;
 	}
 }
 
-int compararFalecimento(Obituario *obit){
-	if( obit[gMortos].dataMorte.ano < obit[gMortos].dataNascimento.ano ){
+int compareDeath(Obtuary *obit)
+{
+	if (obit[gDeadCount].deathDate.year < obit[gDeadCount].birthDate.year)
+	{
 		return 1;
 	}
-	else if(
-	obit[gMortos].dataMorte.ano == obit[gMortos].dataNascimento.ano
-	&& obit[gMortos].dataMorte.mes < obit[gMortos].dataNascimento.mes ){
+	else if (
+		obit[gDeadCount].deathDate.year == obit[gDeadCount].birthDate.year && obit[gDeadCount].deathDate.month < obit[gDeadCount].birthDate.month)
+	{
 		return 1;
 	}
-	else if(
-	obit[gMortos].dataMorte.ano == obit[gMortos].dataNascimento.ano
-	&& obit[gMortos].dataMorte.mes == obit[gMortos].dataNascimento.mes 
-	&& obit[gMortos].dataMorte.dia < obit[gMortos].dataNascimento.dia ){
+	else if (
+		obit[gDeadCount].deathDate.year == obit[gDeadCount].birthDate.year && obit[gDeadCount].deathDate.month == obit[gDeadCount].birthDate.month && obit[gDeadCount].deathDate.day < obit[gDeadCount].birthDate.day)
+	{
 		return 1;
 	}
-	
+
 	return 0;
 }
 
-int checarDatas(int dia, int mes, int ano){
-	if(dia<1 || dia>31){
+int checkDates(int day, int month, int year)
+{
+	if (day < 1 || day > 31)
+	{
 		return 1;
 	}
-	else if(mes<1 || mes>12){
+	else if (month < 1 || month > 12)
+	{
 		return 1;
 	}
-	else if(dia>28 && mes==2){
+	else if (day > 28 && month == 2)
+	{
 		return 1;
 	}
-	else if(dia>30 && (mes==4 || mes==6 || mes==9 || mes==11)){
+	else if (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11))
+	{
 		return 1;
 	}
-	
+
 	return 0;
 }
 
-void listarTodos(Obituario *obit){
-	if(gMortos == 0){
-		printf("\nSEM REGISTROS NO MOMENTO.\n");
-	}else{
+void listAll(Obtuary *obit)
+{
+	if (gDeadCount == 0)
+	{
+		printf("\nNO DATA AT THE MOMENT.\n");
+	}
+	else
+	{
 		printf("\n-------------------------------------------\n");
-    	printf("\t     TELA DE LISTAGEM\n");
-    	printf("-------------------------------------------\n");
-		for(int i=0;i<gMortos;i++){
-			printf("\n|Morto %d|\n",i);
-			printf("-Nome: %s\n",obit[i].nomeMorto);
-			printf("-Data de nascimento: %d/%d/%d\n",obit[i].dataNascimento.dia,
-			obit[i].dataNascimento.mes,obit[i].dataNascimento.ano);
-			printf("-Data da morte: %d/%d/%d\n",obit[i].dataMorte.dia,
-			obit[i].dataMorte.mes,obit[i].dataMorte.ano);
-			printf("-Causa da morte: %s\n",obit[i].causaMorte);
-			printf("-Descrição: %s\n",obit[i].descricaoMorto);
+		printf("\t     LIST ALL SCREEN\n");
+		printf("-------------------------------------------\n");
+		for (int i = 0; i < gDeadCount; i++)
+		{
+			printf("\n|Deceased %d|\n", i);
+			printf("- Name: %s\n", obit[i].deadName);
+			printf("- Birth date: %d/%d/%d\n", obit[i].birthDate.day,
+				   obit[i].birthDate.month, obit[i].birthDate.year);
+			printf("- Death date: %d/%d/%d\n", obit[i].deathDate.day,
+				   obit[i].deathDate.month, obit[i].deathDate.year);
+			printf("- Death cause: %s\n", obit[i].deathCause);
+			printf("- Description: %s\n", obit[i].description);
 		}
 	}
 }
 
-void carregarRegistrosParaMemoria(Obituario *obit){
-    FILE *f;
-    f = fopen("obituario.bin","rb");
-    if( f == NULL ){
-        printf("*Falha ao abrir arquivo.\n(Lembre de criar 'obituario.bin' manualmente)");
-        exit(EXIT_FAILURE);
-    }
-    
-    gMortos = fread(obit,sizeof(Obituario),MAX_MORTOS,f);
-    printf("Registros Lidos = %zd\n",gMortos);
-    
-    if(gMortos >= MAX_MORTOS){
-    	printf("*AVISO: o obituário está cheio.\n");
+void loadRecordsToProgramMemory(Obtuary *obit)
+{
+	FILE *f;
+	f = fopen("obtuary.bin", "rb");
+	if (f == NULL)
+	{
+		printf("*Failed to open file.\n(Remember to create 'obtuary.bin' manually)\n");
+		exit(EXIT_FAILURE);
 	}
-    
-    fclose(f);
+
+	gDeadCount = fread(obit, sizeof(Obtuary), MAX_DEAD, f);
+	printf("Loaded Data = %zd\n", gDeadCount);
+
+	if (gDeadCount >= MAX_DEAD)
+	{
+		printf("*WARNING: obtuary is full.\n");
+	}
+
+	fclose(f);
 }
 
-void limparBuffer(void){
-    char c;
-    while( (c=getchar()) != '\n' && c != EOF );
+void cleanBuffer(void)
+{
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF)
+		;
 }
-
